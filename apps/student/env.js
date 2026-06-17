ae46f7c4d9b0a75882ef0372bf3ec68cb378c57e
Syed Shariar Ahmed <shariar241-35-165@diu.edu.bd>
2026-06-17 17:11:21 +0600

    chore: add real VAPID key

--- apps/student/env.js
+++ apps/student/env.js
@@ -7,5 +7,5 @@ window.ENV = {
   FIREBASE_STORAGE_BUCKET:      'project-anchor-76170.firebasestorage.app',
   FIREBASE_MESSAGING_SENDER_ID: '915588225145',
   FIREBASE_APP_ID:              '1:915588225145:web:1a7bb87daa31a1c4fb5ca8',
-  FIREBASE_VAPID_KEY:           'YOUR_VAPID_KEY_HERE',
+  FIREBASE_VAPID_KEY:           'BD_VTCOQZL46RqdwJ6l1cvpg9OqpY7AuNnbgpND5Eli94hup-ByY5oLWPQGegBOBvXiI8f1ewtAut0ByucuMby4',
 };
